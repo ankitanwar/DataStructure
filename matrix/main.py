ed2c782b9e646e6da3b99abe24f0aade5c7435ae
Ankit Tanwar <tanwariam@gmail.com>
2021-02-01 21:34:12 +0530

    matirx

--- matrix/main.py
+++ matrix/main.py
@@ -1,32 +1,36 @@
-class Solution:
-    def calcOne(self,matrix,low,high):
-        if high>=low:
-            mid=int((low+high)/2)
-            if (mid==0 or matrix[mid-1]==0) and matrix[mid]==1:
-                return mid
-            elif matrix[mid]==1:
-                return self.calcOne(matrix,low,mid-1)
-            else:
-                return self.calcOne(matrix,mid+1,high)
-        return -1
-    def rowWithMax1s(self,arr, n, m):
-		# code here
-        currentAns=0
-        currentRow=-1
-        length=len(arr[0])-1
-        for i in range(len(arr)):
-            if currentAns==m:
-                return currentRow
-            t=length-currentAns
-            if arr[i][t]==1:
-                t2=self.calcOne(arr[i],0,length)
-                if length+1-t2>currentAns:
-                    currentAns=length+1-t2
-                    currentRow=i
-        return currentRow
+def sprialMatrix(matrix):
+    LtoR=0
+    topToBottom=len(matrix[0])
+    bottomToLeft=len(matrix)
+    bottomTOUp=len(matrix)
+    ans=[]
+    while LtoR<bottomTOUp:
+        for i in range(topToBottom):
+            ans.append(matrix[LtoR][i])
+        LtoR+=1
+        for i in range(LtoR,bottomToLeft):
+            ans.append(matrix[i][bottomToLeft])
+        topToBottom-=1
+        for i in range(bottomToLeft-1,bottomTOUp,-1):
+            ans.append(ans[bottomToLeft][i])
+        for i in range(bottomToLeft,LtoR,-1):
+            print("The value is ",bottomTOUp,i)
+            matrix[i][bottomTOUp]
+        bottomToLeft-=1
+        bottomTOUp+=1
+    
+    print("The final ans is",ans)
+
+
+
 mat=[
-    [0,0,1,1,1]
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
 ]
-s=Solution()
-ans=s.rowWithMax1s(mat,1,5)
-print(ans)
+
+sprialMatrix(mat)
+
+
+
