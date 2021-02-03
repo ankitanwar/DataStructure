0e67dd844821492f9652508d5f6ac19ba2724540
Ankit Tanwar <tanwariam@gmail.com>
2021-02-03 21:48:01 +0530

    matrix

--- matrix/main.py
+++ matrix/main.py
@@ -1,36 +1,92 @@
-def sprialMatrix(matrix):
-    LtoR=0
-    topToBottom=len(matrix[0])
-    bottomToLeft=len(matrix)
-    bottomTOUp=len(matrix)
-    ans=[]
-    while LtoR<bottomTOUp:
-        for i in range(topToBottom):
-            ans.append(matrix[LtoR][i])
-        LtoR+=1
-        for i in range(LtoR,bottomToLeft):
-            ans.append(matrix[i][bottomToLeft])
-        topToBottom-=1
-        for i in range(bottomToLeft-1,bottomTOUp,-1):
-            ans.append(ans[bottomToLeft][i])
-        for i in range(bottomToLeft,LtoR,-1):
-            print("The value is ",bottomTOUp,i)
-            matrix[i][bottomTOUp]
-        bottomToLeft-=1
-        bottomTOUp+=1
-    
-    print("The final ans is",ans)
-
-
-
-mat=[
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
-]
-
-sprialMatrix(mat)
-
-
-
+import math
+from math import gcd
+import os
+import sys
+from io import BytesIO, IOBase
+ 
+ 
+# Input Functions
+ 
+def arr_input():
+    return list(map(int, input().split()))
+ 
+ 
+def mul_input():
+    return map(int, input().split())
+ 
+ 
+def int_inp():
+    return int(input())
+def str_input():
+    return str(input())
+ 
+ 
+# New Imports
+ 
+ 
+def solution():
+    pass
+ 
+ 
+def main():
+    testcases = int(input())
+    for _ in range(testcases):
+        solution()
+ 
+ 
+# region fastio
+ 
+BUFSIZE = 8192
+ 
+ 
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+# endregion
+ 
+ 
+if __name__ == "__main__":
+    main()
